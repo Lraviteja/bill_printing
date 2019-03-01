51fd12ec5ffee366a649181dc31387a161f84b2d
Lraviteja <47987132+Lraviteja@users.noreply.github.com>
2019-03-01 13:40:43 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,30 @@
+
+#creating_a_list
+a=['banaba','12','magos','6','apples','20'] #a_defines_list_variable
+print(a)
+
+#creating_a_dynamic_list
+x = []
+n = 10
+for x in range(0, n): 
+    print(x)
+
+#creating_a_list_by_8th_position_is_none
+b=list(('ravi','raju','teja','venkat','21','20','22','25',' ','class'))
+print(b)
+
+#replace_the_7th_position
+list=['string','int','char','dir','doc','variable','comment','data','syntax']
+list[7]=' '
+print(list)
+
+#creating_a_list_with_length_10
+my_list=['maths','physics','chemistry','biology','zoologu','social','history','economics','civics','auditing']
+print(len(my_list))
+
+#sorting_a_list
+bikes=['ninja','royal enfield','fz','r15']
+bikes.sort(),print(bikes)
+
+#reversing_a_list
+bikes.reverse(),print(bikes)
