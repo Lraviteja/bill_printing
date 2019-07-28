2d30d39f97113aa0e55c16cb947f10c8dc3ffbcf
Lraviteja <47987132+Lraviteja@users.noreply.github.com>
2019-07-29 01:34:12 +0530

    Add files via upload

--- list.py
+++ list.py
@@ -1,46 +1,12 @@
+'''
+remove any number from a list
+'''
 
-#creating_a_list
-a=['banaba','12','magos','6','apples','20'] #a_defines_list_variable
-print('created_list_is  ',a,'\n')
-
-#creating_a_dynamic_list
-x = []
-n = 10
-for x in range(0, n): 
-    print('dynamic_ldt_id  ',x,'\n')
-
-#creating_a_list_by_8th_position_is_none
-b=list(('ravi','raju','teja','venkat','21','20','22','25','none','class'))
-print('8th_position_reeplaced_with_none  ',b,'\n')
-
-#replace_the_7th_position
-lst=['string','int','char','dir','doc','variable','comment','data','syntax']
-lst[7]=' '
-print('7th_positiomn_replaced_with_space ',lst,'\n')
-
-#creating_a_list_with_length_10
-my_list=['maths','physics','chemistry','biology','zoologu','social','history','economics','civics','auditing']
-print('10_characters_list_is  ',len(my_list),'\n')
-
-#sorting_a_list
-bikes=['ninja','royal enfield','fz','r15']
-bikes.sort(),print('sorted_looist_is  ',bikes,'\n')
-
-#reversing_a_list
-bikes.reverse()
-print('reverse_list_is  ',bikes,'\n')
-
-#index_of_list
-cls=['name','class','gender','sec','age']
-print('index_is  ',cls.index('sec'),'\n')
-
-#remove_nth_elemnt
-cls.remove('sec')
-print('list_after_removing  ',cls,'\n')
-
-#cloning_a_list
-cls1 = cls.copy()
-print('cloned_list_is  ',cls1,'\n')
-
-#slice_btwn_two_list
-print('sluced_list_is  ',a[3:6],'\n')
+a = [12,24,35,24,88,120,155]
+print('Before:',a)
+i = int(input('Enter any number:'))
+j=i
+for i in a:
+    if i == j:
+        a.remove(j)
+print(a)
